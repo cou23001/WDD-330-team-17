fa85b793f62552cacf19b35d7c0c31ffde887539
cou23001 <cou23001@byui.edu>
2024-01-10 23:12:44 -0700

    Add Item to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,20 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  // current local storage
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
+  
+  // Check if array or initialize
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  // Add product
+  cart.push(product);
+
+  // Update the cart
+  localStorage.setItem("so-cart", JSON.stringify(cart));
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
