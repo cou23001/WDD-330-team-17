5e328a1dc4a9fc97b8bca5f448e902c1c8184bfb
cou23001 <cou23001@byui.edu>
2024-01-10 22:05:19 -0700

    Add Item to Cart Broken

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,20 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  // Get the existing cart from local storage
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
+  
+  // Ensure cart is an array or initialize an empty array
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  // Add the new product to the cart
+  cart.push(product);
+
+  // Update the cart in local storage
+  localStorage.setItem("so-cart", JSON.stringify(cart));
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
