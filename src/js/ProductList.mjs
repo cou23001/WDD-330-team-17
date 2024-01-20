b8c955c5b763d727af992c8ba59e7aa3314d3860
Cesar Coutino <ccoutino@gmail.com>
2024-01-20 14:02:51 -0700

    Remove logs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -21,7 +21,6 @@ export default class ProductList {
     }
     async init() {
         const list = await this.dataSource.getData();
-        console.log('list->',list);
         this.renderList(list.filter(this.filterById));
     }
     renderList(list) {
