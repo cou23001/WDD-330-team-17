a3ecfb12af7bfe96fe3f1579f976b724593014f6
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-02-03 12:05:28 -0700

    Core and step1 stretch ready

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,15 +3,15 @@ import CheckoutProcess from './CheckoutProcess.mjs';
 
 loadHeaderFooter();
 
-const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+const myCheckout = new CheckoutProcess('so-cart', '.checkout-summary');
 myCheckout.init();
 
 document
   .querySelector("input[name='zip']")
-  .addEventListener("blur", myCheckout.calculateOrderTotal.bind(myCheckout));
+  .addEventListener('blur', myCheckout.calculateOrderTotal.bind(myCheckout));
 
 // listening for click on the button
-document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+document.querySelector('#checkoutSubmit').addEventListener('click', (e) => {
   e.preventDefault();
 
   myCheckout.checkout();
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,4 +1,4 @@
-import { loadHeaderFooter, getParam } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from './utils.mjs';
 import ExternalServices from './ExternalServices.mjs';
 import ProductList from './ProductList.mjs';
 import Alert from './Alert.mjs';
@@ -6,18 +6,17 @@ import Alert from './Alert.mjs';
 loadHeaderFooter();
 
 const category = getParam('category');
-const product = getParam('product')
+const product = getParam('product');
 const element = document.querySelector('.product-list');
 
 const dataSource = new ExternalServices();
 
 if (product === '' || product === null) {
-    const listing = new ProductList(category, dataSource, element);
-    listing.init();
-
+  const listing = new ProductList(category, dataSource, element);
+  listing.init();
 } else {
-    const listing = new ProductList(category, dataSource, element);
-    listing.productsByName(product);
+  const listing = new ProductList(category, dataSource, element);
+  listing.productsByName(product);
 }
 
 const listing = new ProductList(category, dataSource, element);
