73668cfc7362f43ec678b678d1c636fe9f677678
Daviles82 <avileshouston@live.com>
2024-02-04 14:27:20 -0600

    trello card for week 4

--- src/js/main.js
+++ src/js/main.js
@@ -3,32 +3,30 @@ import { getLocalStorage, loadHeaderFooter } from './utils.mjs';
 loadHeaderFooter();
 
 function getCategory() {
-    const category = document.getElementsByName('category');
+  const category = document.getElementsByName('category');
 
-    for (var i = 0; i < category.length; i++) {
-        if (category[i].checked) {
-            //console.log("Checked value:", category[i].value);
-            return category[i].value;
-        }
+  for (var i = 0; i < category.length; i++) {
+    if (category[i].checked) {
+      //console.log("Checked value:", category[i].value);
+      return category[i].value;
     }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-    const searchForm = document.getElementById('product-search');
-    const searchInput = document.getElementById('product');
-  
-    searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+  const searchForm = document.getElementById('product-search');
+  const searchInput = document.getElementById('product');
 
-        const selectedCategory = getCategory();
-        const query = searchInput.value.trim();
-        window.location.href = `product-listing/index.html?category=${selectedCategory}&product=${query}`;
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-        // const dataSource = new ExternalServices();
-        
-        // const listing = new ProductList(selectedCategory, dataSource, element);
-        // listing.productsByName(query);
+    const selectedCategory = getCategory();
+    const query = searchInput.value.trim();
+    window.location.href = `product-listing/index.html?category=${selectedCategory}&product=${query}`;
 
-    })
+    // const dataSource = new ExternalServices();
 
-})
+    // const listing = new ProductList(selectedCategory, dataSource, element);
+    // listing.productsByName(query);
+  });
+});
